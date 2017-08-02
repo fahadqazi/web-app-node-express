fdfd8ba2c628e5fe69a04e1b34cb674a79c9f6ae
Fahad Qazi <fahadqazi@gmail.com>
2017-08-02 22:55:45 +0100

    adding gulp and code quality/linting

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+"use strict";
 var express = require('express');
 
 var app = express();
 
-var port = 5000; 
+var port = 5000;
 
 //sets up middleware
 //used by express first, before it does anything else
 app.use(express.static('public'));
-app.use(express.static('src/views'))
+app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
   res.send('hello books!');
 });
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
   console.log('Running on port: ' + port);
-})
+});
