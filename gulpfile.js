cceac196326694e479beb677b3ade7612e353c9c
Fahad Qazi <fahadqazi@gmail.com>
2017-08-02 23:19:52 +0100

    adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-var gulp = require('gulp');
+var gulp = require( 'gulp' );
+var eslint = require( 'gulp-eslint' );
+
+var jsFiles = [ '*.js', 'src/**/*.js' ];
+
+gulp.task( 'style', () =>  {
+    return gulp.src( jsFiles )
+    .pipe( eslint() );
+} );
+
