8ff8ac8953f9a8eaa3bd70d571c9aedf1b1cafa2
Fahad Qazi <fahadqazi@gmail.com>
2017-08-04 00:05:31 +0100

    adding wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,20 @@ var eslint = require( 'gulp-eslint' );
 var jsFiles = [ '*.js', 'src/**/*.js' ];
 
 gulp.task('lint', function () {
-  return gulp.src( jsFiles ).pipe(eslint({
-    'rules':{
-        'quotes': [1, 'single'],
-        'semi': [1, 'always']
-    }
-  }))
+  return gulp.src( jsFiles ).pipe(eslint('.eslintrc'))
   .pipe(eslint.format())
   // Brick on failure to be super strict
   .pipe(eslint.failOnError());
+});
+
+gulp.task('inject', function () {
+  var wiredep = require('wiredep').stream;
+  var options = {
+    bowerJson: require('./bower.json'),
+    directory: './public/lib'
+  };
+
+  return gulp.src('./src/views/*.html')
+    .pipe(wiredep(options))
+    .pipe(gulp.dest('./src/views'));
 });
